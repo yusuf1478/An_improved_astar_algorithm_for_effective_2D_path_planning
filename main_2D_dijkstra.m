c6dc75fa5186de15021ab670a75485a61f704f2f
Mustafa Yusuf Yıldırım <96247195+yusuf1478@users.noreply.github.com>
2026-06-24 10:06:56 +0300

    Add files via upload

---
+++ main_2D_dijkstra.m
@@ -0,0 +1,50 @@
+
+% This project builds upon the A* path planning implementation 
+% from the FOMR-1 project (https://github.com/joedavidbuilds/FOMR-1). 
+% Significant modifications and improvements were introduced by 
+% Rustu Akay & Mustafa Yusuf Yildirim to improve path planning efficiency.
+% akay@erciyes.edu.tr
+% yusufyildirim@ohu.edu.tr 
+
+clc;
+clear;
+close all;
+
+
+% number of nodes 
+ns=50;
+map=map_definition();
+
+
+% generate random nodes
+[map, nodelocation]= generate_node(map,ns);
+
+
+% create undirected graph and its edges
+[undirectedGraph,unedges]=generate_undirected_graph(map,nodelocation);
+
+
+% define start and end point of simulation
+% startp=[5, 29];
+% endp=[29, 20];
+startp=map.startp;
+endp=map.endp;
+
+
+% add start and end location as a new 2 nodes in undirected map.
+% n+1 th node is start point, n+2th node is end point
+[extungraph,exnodelocation,exunedges ]=addstartendpoint2ungraph(map,undirectedGraph,nodelocation,unedges,startp,endp);
+exundnodIndex=[1:ns+2];
+snodeund=ns+1;
+enodeund=ns+2;
+
+
+% optimal path with dijkstra on undirectional map
+[Route Cost] = dijkstra(extungraph,exnodelocation,snodeund);
+if ~isempty(Route)
+    rt=Route{enodeund};
+    dijkstra_route=exnodelocation(rt,:);
+    cost=pathcost(dijkstra_route);
+    close all;
+    drawRoute('Dijkstra',snodeund,enodeund,exnodelocation,exundnodIndex,exunedges,rt,cost);
+end
---
+++ main_2D_dynamic_programming.m
@@ -0,0 +1,48 @@
+
+% This project builds upon the A* path planning implementation 
+% from the FOMR-1 project (https://github.com/joedavidbuilds/FOMR-1). 
+% Significant modifications and improvements were introduced by 
+% Rustu Akay & Mustafa Yusuf Yildirim to improve path planning efficiency.
+% akay@erciyes.edu.tr
+% yusufyildirim@ohu.edu.tr 
+
+clc;
+clear;
+close all;
+
+
+% number of nodes 
+ns=50;
+map=map_definition();
+
+
+% generate random nodes
+[map, nodelocation]= generate_node(map,ns);
+
+
+% create undirected graph and its edges
+[undirectedGraph,unedges]=generate_undirected_graph(map,nodelocation);
+
+
+% define start and end point of simulation
+% startp=[5, 29];
+% endp=[29, 20];
+startp=map.startp;
+endp=map.endp;
+
+
+% add start and end location as a new 2 nodes in undirected map.
+% n+1 th node is start point, n+2th node is end point
+[extungraph,exnodelocation,exunedges ]=addstartendpoint2ungraph(map,undirectedGraph,nodelocation,unedges,startp,endp);
+exundnodIndex=[1:ns+2];
+snodeund=ns+1;
+enodeund=ns+2;
+
+% optimal path with dynamic programming on undirectional map
+[parent, Route]=dynamicpathplanning(extungraph,exnodelocation,exundnodIndex,snodeund,enodeund);
+if ~isempty(Route)
+    dpundirected_route=exnodelocation(Route,:);
+    cost=pathcost(dpundirected_route);
+    drawRoute('Dynamic Programming',snodeund,enodeund,exnodelocation,exundnodIndex,exunedges,Route,cost);
+end
+
